3af794f2893f756fc4e2b777f87b3737757aa62d
Oleksii Burdin <inscopix-oburdin@github.com>
2017-12-02 22:07:01 +0200

    bdd

--- tests/test_purchase.py
+++
@@ -1,6 +0,0 @@
-from pytest_bdd import scenario, given, when, then
-from item_open import *
-
-@scenario('../features/success.feature', 'Success message with order no')
-def test_purchase():
-    pass
