9dd189bfdf9e789327e28563f051e4c06c35b02c
Oleksii Burdin <inscopix-oburdin@github.com>
2017-12-03 20:15:09 +0200

    add a flask webserver

--- api/app.py
+++ api/app.py
@@ -53,6 +53,18 @@ class User(db.Model):
     def generate_auth_token(self, expiration=600):
         s = Serializer(app.config['SECRET_KEY'], expires_in=expiration)
         return s.dumps({'id': self.id})
+	
+	    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except SignatureExpired:
+            return None # valid token, but expired
+        except BadSignature:
+            return None # invalid token
+        user = User.query.get(data['id'])
+        return user
 
         
 @app.route('/todo/api/v1.0/users', methods = ['POST'])
